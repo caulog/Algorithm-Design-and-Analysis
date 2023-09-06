d972ed63d32e42d3b7ea863f65d7e7e5af1137f1
caulog <caulog20@wfu.edu>
2023-09-06 12:32:30 -0400

    working recursive algorithm

---
+++ recursive_division_algorithm.java
@@ -0,0 +1,35 @@
+public class recursive_division_algorithm{
+    public static void main(String[] args){
+        int[] ans = divide(13, 2);
+        
+        System.out.println(ans[0] + ", " + ans[1]);
+    }
+
+    public static int[] divide(int x, int y){
+        int[] result = {0,0};
+
+        if(x == 0){
+            result[0] = 0;
+            result[1] = 0;
+            return result;
+        }
+
+        result = divide((int)(Math.floor(x/2)), y);
+
+        int q = 2*result[0];
+        int r = 2*result[1];
+
+        if(x%2 != 0){
+            r = r+1;
+        }
+
+        if(r >= y){
+            r = r-y;
+            q = q+1;
+        }
+
+        result[0] = q;
+        result[1] = r;
+        return result;
+    }
+}
